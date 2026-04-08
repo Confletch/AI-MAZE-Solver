a52b015da8fcdbcf7c917c7d68678e9925d6b91d
Confletch <fletchc9@my.erau.edu>
2026-04-08 11:01:51 -0400

    added comments to code
    comments to assist team members and new users

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from terminalOutput import TerminalOutput
 
 
 def choose_maze():
+    # maze files which the user can pick from for running the search methods
     maze_files = [
         "mazes/maze1.txt",
         "mazes/maze2.txt",
@@ -11,6 +12,8 @@ def choose_maze():
         "mazes/maze4.txt"
     ]
 
+    # provides the list of mazes the user can choose from
+    # and take's their input (must be a valid choice)
     print("Available Mazes:")
     for i, maze_file in enumerate(maze_files, start=1):
         print(f"{i}. {maze_file}")
@@ -30,22 +33,27 @@ def main():
     maze_file = choose_maze()
     print(f"\nSelected Maze: {maze_file}")
 
-    maze = Maze(maze_file)
-    output = TerminalOutput()
-    solver = Solver(maze)
+    maze = Maze(maze_file) # creates the maze object
+    output = TerminalOutput() # creates an object to handle printing the maze and results
+    solver = Solver(maze) # creates the solver object
 
+    # prints the origional maze before solving
+    # just so the user can see a nice before and after
     print("\nOriginal Maze:")
     output.display_maze(maze.grid)
 
+    # runs all three search algorithms and stores each result
     results = [
         solver.bfs(),
         solver.greedy(),
         solver.astar()
     ]
 
+    # goes through each search algorithms results
     for result in results:
         print(f"\n{result['algorithm']} Results:")
 
+        # if path was found, shows the explored nodes, final path, and stats from search
         if result["path_found"]:
             output.display_explored_nodes(
                 maze.grid,
@@ -54,6 +62,7 @@ def main():
             )
             print()
 
+            # shows the explored nodes
             output.display_explored_only(
                 maze.grid,
                 result["explored_nodes"],
@@ -61,18 +70,20 @@ def main():
             )
             print()
 
-            output.display_solution(maze.grid, result["path"])
-            output.display_stats(result)
+            output.display_solution(maze.grid, result["path"]) # shows the final path only
+            output.display_stats(result) # shows the stats from the algorithm
         else:
+            # shows if there was no found path
+            # still shows the explored nodes just no path
             print("No path found.")
             output.display_explored_nodes(
                 maze.grid,
                 result["explored_nodes"]
             )
-            output.display_stats(result)
+            output.display_stats(result) # still shows the stats from search
 
+    # simple summary table to show comparision of all three algorithms
     output.display_comparison_summary(results)
-
 
 if __name__ == "__main__":
     main()
